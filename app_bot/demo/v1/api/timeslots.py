a715c0044aa363916435b7f02bec7023e3e85e31
Antonio Daniswara <antonio.daniswara@gmail.com>
2019-03-29 19:43:39 +1100

    done, check timeslot

--- app_bot/demo/v1/api/timeslots.py
+++ app_bot/demo/v1/api/timeslots.py
@@ -5,11 +5,21 @@ from flask import request, g
 
 from . import Resource
 from .. import schemas
-
+from requests import get
 
 class Timeslots(Resource):
 
     def get(self):
         print(g.args)
+        
+        url = "http://0.0.0.0:3000/v1/appointments/byDoctor?name={}".format(g.args['dentist_name'])
+        output = {"redirect_to_blocks": ["book.confirm"]}
+        resp = get(url)
+        # print(resp)
+        for res in resp.json():
+            # print(res)
+            if res['date'][:10] == g.args['booking_date'] and res['time'] == g.args['booking_time']:
+                output = {"redirect_to_blocks": ["book.timeslot_fail"]}
+                break
 
-        return {}, 200, None
+        return output, 200, None
